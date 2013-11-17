86b7f9945f1ea4448011954fd5878140cd8affd1
Erwin Mueller <erwin.mueller@deventm.org>
2013-11-17 15:06:32 +0100

    Update license headers
    #2737

--- fractions-calculator-cli/src/main/java/com/anrisoftware/fractions/calculator/parser/CalculationParser.java
+++ fractions-calculator-cli/src/main/java/com/anrisoftware/fractions/calculator/parser/CalculationParser.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of fractions-calculator-cli.
+ *
+ * fractions-calculator-cli is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * fractions-calculator-cli is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * fractions-calculator-cli. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.fractions.calculator.parser;
 
 import javax.inject.Inject;
